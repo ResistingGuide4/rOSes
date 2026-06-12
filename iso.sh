9a2ae32ec8cadaf6072fccb96b5f9a9b78311cbc
ResistingGuide4 <thomasmrose11@gmail.com>
2026-06-12 13:46:17 -0400

    Rename myos to roses

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/roses.kernel isodir/boot/roses.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "roses" {
+	multiboot /boot/roses.kernel
 }
 EOF
-grub2-mkrescue -o myos.iso isodir
+grub2-mkrescue -o roses.iso isodir
