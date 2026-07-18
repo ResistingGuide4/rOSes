bbdb03c9b57456b514e71666e4302ca142c38142
ResistingGuide4 <thomasmrose11@gmail.com>
2026-07-18 17:50:34 -0400

    Move struct definitions to specific header files and get mem map from GRUB

--- kernel/arch/i386/earlyinit.c
+++ kernel/arch/i386/earlyinit.c
@@ -2,99 +2,13 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#include <kernel/gdt.h>
+#include <kernel/multiboot2.h>
+
 extern void reloadSegments(void);
 extern uint32_t stack_top;
 
-typedef struct __attribute__((packed)) {
-    uint32_t link;
-    uint32_t esp0;
-    uint32_t ss0;
-    uint32_t esp1;
-    uint32_t ss1;
-    uint32_t esp2;
-    uint32_t ss2;
-    uint32_t cr3;
-    uint32_t eip;
-    uint32_t eflags;
-    uint32_t eax;
-    uint32_t ecx;
-    uint32_t edx;
-    uint32_t ebx;
-    uint32_t esp;
-    uint32_t ebp;
-    uint32_t esi;
-    uint32_t edi;
-    uint32_t es;
-    uint32_t cs;
-    uint32_t ss;
-    uint32_t ds;
-    uint32_t fs;
-    uint32_t gs;
-    uint32_t ldtr;
-    uint16_t reserved; // To move iobp 2 bytes forward
-    uint16_t iopb;
-    uint32_t ssp;
-} tss_t;
-
 tss_t tss_1;
-
-// Each define here is for a specific flag in the descriptor.
-// Refer to the intel documentation for a description of what each one does.
-#define SEG_DESCTYPE(x)  ((x) << 0x04) // Descriptor type (0 for system, 1 for code/data)
-#define SEG_PRIV(x)     (((x) &  0x03) << 0x05)   // Set privilege level (0 - 3)
-#define SEG_PRES(x)      ((x) << 0x07) // Present
-#define SEG_SAVL(x)      ((x) << 0x0C) // Available for system use
-#define SEG_LONG(x)      ((x) << 0x0D) // Long mode
-#define SEG_SIZE(x)      ((x) << 0x0E) // Size (0 for 16-bit, 1 for 32)
-#define SEG_GRAN(x)      ((x) << 0x0F) // Granularity (0 for 1B - 1MB, 1 for 4KB - 4GB)
- 
-#define SEG_DATA_RD        0x00 // Read-Only
-#define SEG_DATA_RDA       0x01 // Read-Only, accessed
-#define SEG_DATA_RDWR      0x02 // Read/Write
-#define SEG_DATA_RDWRA     0x03 // Read/Write, accessed
-#define SEG_DATA_RDEXPD    0x04 // Read-Only, expand-down
-#define SEG_DATA_RDEXPDA   0x05 // Read-Only, expand-down, accessed
-#define SEG_DATA_RDWREXPD  0x06 // Read/Write, expand-down
-#define SEG_DATA_RDWREXPDA 0x07 // Read/Write, expand-down, accessed
-#define SEG_CODE_EX        0x08 // Execute-Only
-#define SEG_CODE_EXA       0x09 // Execute-Only, accessed
-#define SEG_CODE_EXRD      0x0A // Execute/Read
-#define SEG_CODE_EXRDA     0x0B // Execute/Read, accessed
-#define SEG_CODE_EXC       0x0C // Execute-Only, conforming
-#define SEG_CODE_EXCA      0x0D // Execute-Only, conforming, accessed
-#define SEG_CODE_EXRDC     0x0E // Execute/Read, conforming
-#define SEG_CODE_EXRDCA    0x0F // Execute/Read, conforming, accessed
- 
-#define GDT_CODE_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
-                     SEG_PRIV(0)     | SEG_CODE_EXRD
- 
-#define GDT_DATA_PL0 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
-                     SEG_PRIV(0)     | SEG_DATA_RDWR
- 
-#define GDT_CODE_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
-                     SEG_PRIV(3)     | SEG_CODE_EXRD
- 
-#define GDT_DATA_PL3 SEG_DESCTYPE(1) | SEG_PRES(1) | SEG_SAVL(0) | \
-                     SEG_LONG(0)     | SEG_SIZE(1) | SEG_GRAN(1) | \
-                     SEG_PRIV(3)     | SEG_DATA_RDWR
-
-typedef struct __attribute__((packed)) {
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_mid;
-    uint8_t access;
-    uint8_t limit_high : 4;
-    uint8_t flags : 4;
-    uint8_t base_high;
-} gdt_entry_t;
-
-typedef struct __attribute__((packed)) {
-    uint16_t limit;
-    uint32_t base;
-} gdtr_t;
 
 gdt_entry_t gdt[200];
 
@@ -140,7 +54,44 @@ void gdt_init(void) {
     );
 }
 
-void kernel_early_main(void) {
+struct multiboot_tag *multiboot_find_tag(void *mbd, uint32_t type) {
+    /* The multiboot info structure begins with a 32-bit integer
+    *  indicating the total size of the structure. This is then
+    *  followed by a 32-bit reserved region and then by the tags. */
+	struct multiboot_tag *tag = (void *)mbd + 8;
+	while ((void *)tag < (void *)mbd + *((uint32_t *)mbd)) {
+		if (tag->type == type) {
+			return tag;
+		}
+		tag = (void *)tag + tag->size;
+		/* Tags are always aligned on 8-byte boundaries. */
+		if ((uintptr_t)tag % 8 > 0) {
+			tag = (void *)tag + 8 - ((uintptr_t)tag % 8);
+		}
+	}
+	return 0;
+}
+
+void kernel_early_main(void *mbd, uint32_t magic) {
+    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+        panic("invalid magic number!");
+    }
+    
+    struct multiboot_tag_mmap *tag_mmap =
+	    (struct multiboot_tag_mmap *)multiboot_find_tag(
+		mbd, MULTIBOOT_TAG_TYPE_MMAP);
+
+	if (!tag_mmap) {
+		panic("No memory map tag found!\n");
+	}
+
+	struct multiboot_mmap_entry *entry =
+	    (struct multiboot_mmap_entry *)(uintptr_t)tag_mmap->entries;
+	while ((void *)entry < (void *)tag_mmap + tag_mmap->size) {
+		/*kprintf("Start Addr: %x | Length: %x | Type: %i.\n",
+		    entry->addr, entry->len, entry->type);*/
+		entry = (void *)entry + tag_mmap->entry_size;
+	}
 
 }
 
