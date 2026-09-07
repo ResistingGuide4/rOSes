2989b0cf2ec682bc99f273544d16732c4a5d79a7
ResistingGuide4 <thomasmrose11@gmail.com>
2026-09-07 12:57:35 -0400

    Add Kernel Block Allocation to VMM

--- kernel/include/kernel/vmm.h
+++ kernel/include/kernel/vmm.h
@@ -2,14 +2,16 @@
 #define VMM_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
 typedef struct {
     void *addr;
     uint32_t size; //in pages
 } __attribute__((packed)) vmm_block_t;
 
 void vmm_init();
-void *alloc_block(uint32_t size);
-void free_block(void *addr, uint32_t size);
+void *alloc_block(uint32_t size, bool isKernel);
+void free_block(void *addr, uint32_t size, bool isKernel);
 void read_kernel_space();
 void read_user_space();
 void *get_physaddr(void *virtualaddr);
