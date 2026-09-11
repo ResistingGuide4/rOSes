ec1b4490240bd9b274bb816643b0cdedb394f942
ResistingGuide4 <thomasmrose11@gmail.com>
2026-09-11 13:34:44 -0400

    Add Supervisor Bit if is Kernel in alloc_block

--- kernel/arch/i386/vmm.c
+++ kernel/arch/i386/vmm.c
@@ -169,7 +169,8 @@ void *alloc_block(uint32_t size, bool isKernel) {
         if (phys_addr == NULL) {
             return NULL;
         }
-        map_page(phys_addr, virt_addr + i * 0x1000, 0x02);
+        uint8_t supervisor = (0x4 & isKernel << 2);
+        map_page(phys_addr, virt_addr + i * 0x1000, 0x02 | (0x4 & isKernel << 2));
     }
 
     return virt_addr;
